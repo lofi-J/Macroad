47ecdfbdcae84feabc34a9075322c5b8335d916e
lofi <jsj2505@gmail.com>
2025-12-06 16:13:23 +0900

    [init] import path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 function App() {
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
-
   return (
     <main>
       <h1>Welcome to macroad</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import App from "@/app";
+import "@/globals.css";
 import ReactDOM from "react-dom/client";
-import App from "./app";
-import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
