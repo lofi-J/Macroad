65628d6c08e2e25fb81cd61ef6e08580807d8d94
lofi <jsj2505@gmail.com>
2025-12-18 14:15:14 +0900

    [fix] button minor fix

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,22 +1,17 @@
 import { useLayout } from '@/context/layout-provider';
-import { useQuery } from '@tanstack/react-query';
-import { invoke } from '@tauri-apps/api/core';
+import { Button } from './ui/button';
 
 export const Topbar = () => {
   const { sidebarWidth, topbarHeight } = useLayout();
-  const { data, isPending } = useQuery({
-    queryKey: ['SOME_COUNT'],
-    queryFn: () => invoke('get_some_count'),
-  });
 
   return (
     <header
       className="fixed top-0 z-50 border border-l-0"
       style={{ left: `${sidebarWidth}px`, right: 0, height: `${topbarHeight}px` }}
     >
-      <div className="bg-background flex items-center justify-center">
-        {isPending ? <span>Loading...</span> : JSON.stringify(data)}
-      </div>
+      <Button variant="outline" size="sm">
+        Play
+      </Button>
     </header>
   );
 };
